fd514074ec11f357d916699cb566c9cbdf3e73c1
hadrien-pouget <hpouget@hotmail.com>
2021-04-13 09:34:41 +0100

    Bug fix

--- games/__init__.py
+++ games/__init__.py
@@ -2,12 +2,12 @@ from .communication_game import DistInfComms
 from .mem1_game import DistInf, IncompleteFour
 
 ALL_GAMES = ["DistInf", "IncompFour", "DistInfComms"]
-def get_game(name, **kwargs):
-    if name == "DistInf":
+def get_game(game, **kwargs):
+    if game == "DistInf":
         return DistInf(p=kwargs['p'], a=kwargs['a'], oneshot=kwargs['oneshot'])
-    if name == "IncompFour":
+    if game == "IncompFour":
         return IncompleteFour(prior_n=kwargs['prior_n'], oneshot=kwargs['oneshot'])
-    if name == "DistInfComms":
+    if game == "DistInfComms":
         return DistInfComms(p=kwargs['p'], a=kwargs['a'])
     
     raise NotImplementedError("Game not implemented")
