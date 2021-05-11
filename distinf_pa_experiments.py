75b1ba93b6d61fc1ea1544781aa609da7abad708
hadrien-pouget <hpouget@hotmail.com>
2021-05-11 09:46:34 +0100

    Small correction

--- distinf_pa_experiments.py
+++ distinf_pa_experiments.py
@@ -9,7 +9,7 @@ for prior_1 in range(0, 11, 1):
     prior_1_param = [prior_1/10]
     for prior_2 in range(0, 11, 1):
         prior_2_param = [prior_2/10]
-        for a in range(1, 11, 1):
+        for a in [1,2,5,10]:
             config = {
                 "a":a,
                 "game":GAME,
@@ -27,5 +27,5 @@ for prior_1 in range(0, 11, 1):
 
             seed(1234) # pretty sure this isn't like running each experiment alone with this seed
             env = get_game(GAME, prior_1_param=prior_1_param, prior_2_param=prior_2_param, a=a)
-            save_folder = "{}_p{}{}_a{}_{}".format(GAME, prior_1, prior_2, a, 'naive')
+            save_folder = "{}_p{}_{}_a{}_{}".format(GAME, prior_1, prior_2, a, 'naive')
             experiment(env, 'naive', 40, 0.96, 1.0, 100, True, 100, save_folder, config)
